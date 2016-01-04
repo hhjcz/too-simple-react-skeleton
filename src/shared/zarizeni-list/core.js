9e6cbf29c55bff29bd36ecbfcb57013c49e34d35
Jan Haering <hhj@centrum.cz>
2016-01-04 11:37:34 +0100

    Fix state hydration

--- src/shared/zarizeni-list/core.js
+++ src/shared/zarizeni-list/core.js
@@ -1,8 +1,10 @@
 /**
  * Created by hhj on 12/18/15.
  */
-import {List} from 'immutable'
+import { List } from 'immutable'
+
+import { Zarizeni } from '../zarizeni/core'
 
 export function setList(state, seznamZarizeni) {
-  return state.set('seznamZarizeni', List(seznamZarizeni))
+  return state.set('seznamZarizeni', List(seznamZarizeni).map(z => new Zarizeni(z)))
 }
