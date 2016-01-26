3540768687b5f6d4d87e92d5d615bb23d72bcd68
Jan Haering <hhj@centrum.cz>
2016-01-26 10:52:25 +0100

    Fix non existing .env.json file

--- src/shared/lib/loadEnv.js
+++ src/shared/lib/loadEnv.js
@@ -1,11 +1,13 @@
 /** Created by hhj on 1/26/16. */
 export default function loadEnv(envFile) {
-  let env
+  let env = {}
   if (process.env.IS_BROWSER !== true) {
     const fs = require('fs')
     const nodeEnv = process.env.NODE_ENV === 'production' ? 'production' : 'development'
-    const envJson = JSON.parse(fs.readFileSync(envFile, 'utf8'))[nodeEnv]
-    env = { ...envJson, ...process.env }
+    try {
+      const envJson = JSON.parse(fs.readFileSync(envFile, 'utf8'))[nodeEnv]
+      env = { ...envJson, ...process.env }
+    } catch(e) {}
   } else {
     env = process.env
   }
