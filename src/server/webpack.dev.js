abf0195f5eb24ada1fae2c3355b61ba1b03d48a6
Jan Haering <hhj@centrum.cz>
2016-02-05 10:54:53 +0100

    chore: upgrade npm packages

--- src/server/webpack.dev.js
+++ src/server/webpack.dev.js
@@ -1,7 +1,6 @@
 /** Created by hhj on 12/23/15. */
 
 import webpack from 'webpack';
-import assign from 'object-assign';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import prodCfg from './webpack.prod.js';
@@ -9,8 +8,6 @@ import IsomorphicToolsPlugin from '../../node_modules/webpack-isomorphic-tools/p
 import isomorphicToolsConfig from './webpack-isomorphic-tools.config.js';
 
 const isomorphicToolsPlugin = new IsomorphicToolsPlugin(isomorphicToolsConfig)
-
-Object.assign = assign;
 
 // add hot reload feature in dev mode (using babel-plugin-react-transform & react-transform-hmr),
 // rest of babel config taken from .babelrc
@@ -35,58 +32,61 @@ const prefixLoaders = 'style-loader!css-loader!postcss-loader'
 
 // consumed in src/server.js
 export default function(app) {
-  const config = Object.assign(prodCfg, {
-    devtool: 'inline-source-map',
-    entry: {
-      main: [
-        'webpack-hot-middleware/client',
-        './src/client'
-      ]
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'babel',
-          query: BABEL_QUERY
-        },
-        {
-          loader: 'url-loader?limit=100000',
-          test: /\.(gif|jpg|png|woff|woff2|eot|ttf|svg)$/
-        },
-        {
-          test: /\.css$/,
-          loader: prefixLoaders
-        },
-        {
-          test: /\.scss$/,
-          loader: `${prefixLoaders}!sass-loader`
-        },
-        {
-          test: /\.less$/,
-          loader: `${prefixLoaders}!less-loader`
-        },
-        {
-          test: /\.styl$/,
-          loader: `${prefixLoaders}!stylus-loader`
-        }
-      ]
-    },
-    plugins: [
-      new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoErrorsPlugin(),
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('development'),
-          IS_BROWSER: true,
-          SERVER_BASE_URL: JSON.stringify(process.env.SERVER_BASE_URL)
-        }
-      }),
-      isomorphicToolsPlugin.development()
-    ],
-  })
+  const config = {
+    ...prodCfg,
+    ...{
+      devtool: 'inline-source-map',
+      entry: {
+        main: [
+          'webpack-hot-middleware/client',
+          './src/client'
+        ]
+      },
+      module: {
+        loaders: [
+          {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: BABEL_QUERY
+          },
+          {
+            loader: 'url-loader?limit=100000',
+            test: /\.(gif|jpg|png|woff|woff2|eot|ttf|svg)$/
+          },
+          {
+            test: /\.css$/,
+            loader: prefixLoaders
+          },
+          {
+            test: /\.scss$/,
+            loader: `${prefixLoaders}!sass-loader`
+          },
+          {
+            test: /\.less$/,
+            loader: `${prefixLoaders}!less-loader`
+          },
+          {
+            test: /\.styl$/,
+            loader: `${prefixLoaders}!stylus-loader`
+          }
+        ]
+      },
+      plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env': {
+            NODE_ENV: JSON.stringify('development'),
+            IS_BROWSER: true,
+            SERVER_BASE_URL: JSON.stringify(process.env.SERVER_BASE_URL)
+          }
+        }),
+        isomorphicToolsPlugin.development()
+      ],
+    }
+  }
 
   const compiler = webpack(config);
 
