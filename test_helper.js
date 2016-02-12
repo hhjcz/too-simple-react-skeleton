43f4368939f52b88093524fe4ab9b4f0c02e41ea
Jan Haering <hhj@centrum.cz>
2016-02-12 11:17:43 +0100

    feat: add chai-as-promised

--- test_helper.js
+++ test_helper.js
@@ -1,6 +1,7 @@
 /** Created by hhj on 12/17/15. */
 import chai from 'chai'
 import chaiImmutable from 'chai-immutable'
+import chaiAsPromised from 'chai-as-promised'
 import spies from 'chai-spies'
 import loadEnv from './src/server/loadEnv'
 
@@ -8,6 +9,8 @@ loadEnv()
 
 chai.use(chaiImmutable)
 chai.use(spies)
+chai.use(chaiAsPromised)
+chai.should()
 
 // ignore import and require of these extensions on server (in node; deprecated but still functional):
 const ignoreRequireExtensions = ['.scss', '.sass', '.css', '.styl']
