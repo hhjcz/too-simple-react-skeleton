fbd450a5e53e6612e2e9ab8ab83ace17440a52ba
Jan Haering <hhj@centrum.cz>
2016-02-23 10:36:27 +0100

    feat: allow nested actions in mapDispatchToProps

--- src/shared/lib/__tests__/createMapDispatchToProps.spec.js
+++ src/shared/lib/__tests__/createMapDispatchToProps.spec.js
@@ -6,6 +6,7 @@ describe('createMapDispatchToProps', () => {
 
   const firstAction = x => x
   const secondAction = y => y
+  const thirdAction = z => z
   const dispatch = z => `dispatched ${z}`
 
   it('should map dispatch function to props', () => {
@@ -22,18 +23,44 @@ describe('createMapDispatchToProps', () => {
   })
 
   it('should bind actions passed as array', () => {
-    const actions = [{ firstAction, secondAction }, { secondAction, firstAction }]
+    const actions = [{ firstAction, secondAction }, { thirdAction, firstAction }]
     const props = createMapDispatchToProps(actions)(dispatch)
 
     expect(typeof props.actions.firstAction).to.equal('function')
-    expect(props.actions.firstAction('something')).to.equal('dispatched something')
+    expect(props.actions.thirdAction('something')).to.equal('dispatched something')
+  })
+
+  it('should bind nested actions passed as deep object', () => {
+    const actions = { firstGroup: { firstAction, secondAction }, secondGroup: { secondAction, firstAction } }
+    const props = createMapDispatchToProps(actions)(dispatch)
+
+    expect(typeof props.actions.firstGroup.firstAction).to.equal('function')
+    expect(props.actions.firstGroup.secondAction('something')).to.equal('dispatched something')
   })
 
   it('should not duplicate actions (should merge)', () => {
-    const actions = [{ firstAction, secondAction }, { secondAction, firstAction }]
+    const actions = [{ firstAction, secondAction }, { secondAction, firstAction }, { secondAction, firstAction }]
     const props = createMapDispatchToProps(actions)(dispatch)
 
     expect(Object.keys(props.actions).length).to.equal(2)
+  })
+
+  it('should filter out strings', () => {
+    const actions = { firstAction, secondAction, firstString: 'FIRST-STRING' }
+    const props = createMapDispatchToProps(actions)(dispatch)
+
+    expect(Object.keys(props.actions).length).to.equal(2)
+  })
+
+  it('should filter out nested strings', () => {
+    const actions = {
+      firstGroup: { firstAction, secondAction },
+      secondGroup: { secondAction, firstAction },
+      thirdGroup: { firstString: 'FIRST-STRING' }
+    }
+    const props = createMapDispatchToProps(actions)(dispatch)
+
+    expect(Object.keys(props.actions).length).to.equal(3)
   })
 
 })
--- src/shared/lib/createMapDispatchToProps.js
+++ src/shared/lib/createMapDispatchToProps.js
@@ -1,20 +1,26 @@
 /** Created by hhj on 12/28/15. */
 import { bindActionCreators } from 'redux'
-import { Map } from 'immutable'
+import { reduce, isArray } from 'lodash'
 
 /**
  * @param actions
  */
 const createMapDispatchToProps = actions => dispatch => {
-  const creators = Map()
-    .merge(actions)  // when actions is an object
-    .merge(...actions) // when action is an array (called with [actions])
-    .filter(value => typeof value === 'function')
-    .toObject()
+  const boundActions = reduce(actions, (result, value, key) => {
+    if (typeof value === 'string') return result
+    if (isArray(actions)) {
+      /* eslint-disable no-param-reassign */
+      result = { ...result, ...bindActionCreators(value, dispatch) }   // merge array items
+    } else {
+      result[key] = bindActionCreators(value, dispatch)   // add nested actions as object
+    }
+
+    return result
+  }, {})
 
   return ({
     dispatch,
-    actions: bindActionCreators(creators, dispatch)
+    actions: boundActions
   })
 }
 
