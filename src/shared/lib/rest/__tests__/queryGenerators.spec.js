6831499950b5462fe1f61d91b24a225379ced321
Jan Haering <hhj@centrum.cz>
2016-03-03 16:22:54 +0100

    test: add tests for rest query generators

---
+++ src/shared/lib/rest/__tests__/queryGenerators.spec.js
@@ -0,0 +1,38 @@
+/** Created by hhj on 3/3/16. */
+import { expect } from 'chai'
+import { Map } from 'immutable'
+import qs from 'query-string'
+import queryGenerators from '../queryGenerators'
+import Filter from '../../../app/models/Filter'
+import Sort from '../../../app/models/Sort'
+import Pagination from '../../../app/models/Pagination'
+
+describe('rest library queryGenerators', () => {
+  const { fetchAll: collectionGenerator, fetchOne: itemGenerator } = queryGenerators
+
+  it('should generate query params', () => {
+    const someFilter1 = new Filter({ name: 'nejakyFilter1', value: 'hodnotaFiltru1' })
+    const someFilter2 = new Filter({ name: 'nejakyFilter2', value: 'hodnotaFiltru2', comparator: 'equal' })
+    const someFilter3 = new Filter({ name: 'nejakyFilter3', value: false, comparator: 'empty' })
+    const someFilter4 = new Filter({ name: 'nejakyFilter4', value: true, comparator: 'empty' })
+    const filters = Map([[someFilter1.name, someFilter1], [someFilter2.name, someFilter2], [someFilter3.name, someFilter3], [someFilter4.name, someFilter4]])
+    const sort = new Sort({ dir: true, by: 'nejakySort' })
+    const pagination = new Pagination({ page: 66, perPage: 6 })
+
+    const query = collectionGenerator({ filters, sort, pagination })
+    expect(query['nejaky_filter_1-lk']).to.equal('%hodnotaFiltru1%')
+    expect(query.nejaky_filter_2).to.equal('hodnotaFiltru2')
+    // expect(query.nejaky_filter_3).to.equal('')
+    expect(query._sort).to.equal('-nejaky_sort')
+    expect(query.page).to.equal(66)
+    expect(query.per_page).to.equal(6)
+
+    expect(qs.stringify(query)).to.equal('_sort=-nejaky_sort&nejaky_filter_1-lk=%25hodnotaFiltru1%25&nejaky_filter_2=hodnotaFiltru2&nejaky_filter_3-not=&nejaky_filter_4-null&page=66&per_page=6')
+  })
+
+  it('should generate from empty object', () => {
+    const query = collectionGenerator({})
+    expect(query).to.deep.equal({})
+  })
+
+})
