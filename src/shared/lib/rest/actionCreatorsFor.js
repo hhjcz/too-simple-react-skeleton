32b9003a11574664b8db216c66413dd7bbd19da5
Jan Haering <hhj@centrum.cz>
2016-03-08 11:36:09 +0100

    refactor: functional style

--- src/shared/lib/rest/actionCreatorsFor.js
+++ src/shared/lib/rest/actionCreatorsFor.js
@@ -1,19 +1,12 @@
 /** Created by hhj on 2/3/16. */
 
-export function actionCreatorsFor(actionTypes) {
-  const actionCreators = Object.keys(actionTypes).reduce((result, key) => {
-    if (key.indexOf('@') > -1 || key.indexOf('_') > -1) return result
-    const creator = args => ({
-      type: actionTypes[key],
-      ...args
-    })
-    /* eslint-disable no-param-reassign */
-    result[key] = creator
+const isValidActionCreator = key => key.indexOf('@') <= -1 && key.indexOf('_') <= -1
 
+export function actionCreatorsFor(actionTypes) {
+  return Object.keys(actionTypes).filter(isValidActionCreator).reduce((result, key) => {
+    result[key] = args => ({ type: actionTypes[key], ...args })
     return result
   }, {})
-
-  return actionCreators
 }
 
 export default actionCreatorsFor
