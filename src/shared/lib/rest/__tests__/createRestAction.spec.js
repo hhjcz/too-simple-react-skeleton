03d5b575a404a01a31d36d1cbc65176c9328a8a2
Jan Haering <hhj@centrum.cz>
2016-03-15 15:22:43 +0100

    test: for createRestAction

---
+++ src/shared/lib/rest/__tests__/createRestAction.spec.js
@@ -0,0 +1,69 @@
+/** Created by hhj on 3/15/16. */
+/* eslint-disable no-unused-expressions */
+import { expect } from 'chai'
+import createRestAction from '../createRestAction'
+import actionTypesFor from '../actionTypesFor'
+import actionCreatorsFor from '../actionCreatorsFor'
+
+describe('rest lib createRestAction', () => {
+
+  const endpointName = 'povidky'
+  const config = {
+    url: `/${endpointName}/:id`,
+    extraParams: { include: 'subresource.subsubresource', otherExtraParam: 'someValue' },
+    itemTransformer: item => item,
+    defaultState: { someState: { value: 666 } }
+  }
+  const actionTypes = actionTypesFor(endpointName)
+  const actionCreators = actionCreatorsFor(actionTypes)
+  const getState = () => ({ povidky: {} })
+  const dispatch = action => {
+    if (typeof action === 'function') return action({ dispatch, getState, history: {} })
+    return action
+  }
+  const fetchBuffer = {}
+  const fetch = (url, args) => {
+    fetchBuffer.url = url
+    fetchBuffer.args = args
+    return Promise.resolve(url, ...args)
+  }
+  const fnHolder = { fetch, dispatch }
+
+
+  it('should create rest actions', () => {
+    const actions = createRestAction(endpointName, config, actionCreators, fnHolder)
+    expect(typeof actions.fetchAll).to.equal('function')
+    expect(typeof actions.fetchOne).to.equal('function')
+    expect(typeof actions.create).to.equal('function')
+    expect(typeof actions.update).to.equal('function')
+    expect(typeof actions.destroy).to.equal('function')
+  })
+
+  it('should handle params', () => {
+    const actions = createRestAction(endpointName, config, actionCreators, fnHolder)
+    const promise = actions.fetchAll({ params: { someParam: 'someValue' } })
+    const expectedUrl = '/povidky?include=subresource.subsubresource&other_extra_param=someValue&some_param=someValue'
+
+    expect(fetchBuffer.url).to.equal(expectedUrl)
+    expect(fetchBuffer.args.method).to.equal('GET')
+
+    return promise.then(response => {
+      expect(response.meta.lastFetchMark).to.equal(expectedUrl)
+    })
+  })
+
+  it('should handle params & body', () => {
+    const actions = createRestAction(endpointName, config, actionCreators, fnHolder)
+    const promise = actions.update({ params: { someParam: 'someValue' }, body: { someBody: 'someValue' } })
+    const expectedUrl = '/povidky?include=subresource.subsubresource&other_extra_param=someValue&some_param=someValue'
+
+    expect(fetchBuffer.url).to.equal(expectedUrl)
+    expect(fetchBuffer.args.method).to.equal('PATCH')
+    expect(fetchBuffer.args.body).to.equal('{"someBody":"someValue"}')
+
+    return promise.then(response => {
+      expect(response.meta.lastFetchMark).to.equal(expectedUrl)
+    })
+  })
+
+})
