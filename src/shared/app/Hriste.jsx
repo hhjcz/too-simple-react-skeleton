40c3cc51b6a75ab21766b07d2a780c42061540c6
Jan Haering <hhj@centrum.cz>
2016-03-16 16:54:41 +0100

    chore: add staff to Hriste

--- src/shared/app/Hriste.jsx
+++ src/shared/app/Hriste.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Image } from 'react-bootstrap'
 import './fonts.css'
+import MyIcon from '../lib/MyIcon'
 
 export default class Hriste extends React.Component {
   static propTypes = {};
@@ -9,10 +10,16 @@ export default class Hriste extends React.Component {
   render() {
     return (
       <div>
-        <img alt="50x50 placeholder" src={require('./50x50.png')} />
-        <Image alt="50x50 placeholder" src={require('./50x50.png')} circle />
-        <div style={{ fontFamily: 'Roboto' }}>
-          Roboto font famílije
+        <div>
+          <img alt="50x50 placeholder" src={require('./50x50.png')} />
+          <Image alt="50x50 placeholder" src={require('./50x50.png')} circle />
+        </div>
+        <div style={{ fontFamily: 'Roboto', fontSize: '2em', fontWeight: '300' }}>
+          Roboto-300 font family: příliš žluťoučký kůň úpěl dábelské kódy
+        </div>
+        <div style={{ fontWeight: '300' }}>
+          <MyIcon color="red">delete</MyIcon>
+          <MyIcon color="green" style={{ fontSize: '36px' }}>motorcycle</MyIcon>
         </div>
       </div>
     )
