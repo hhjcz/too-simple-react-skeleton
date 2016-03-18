4824d8691530ca07e01594390e50ecfff7b37c44
Jan Haering <hhj@centrum.cz>
2016-03-18 15:12:11 +0100

    chore: hriste view update

--- src/shared/app/Hriste.jsx
+++ src/shared/app/Hriste.jsx
@@ -1,6 +1,7 @@
 /** Created by hhj on 1/12/16. */
 import React from 'react'
 import { Image } from 'react-bootstrap'
+import FontIcon from 'material-ui/lib/font-icon'
 import './fonts.css'
 import MyIcon from '../lib/MyIcon'
 
@@ -17,9 +18,16 @@ export default class Hriste extends React.Component {
         <div style={{ fontFamily: 'Roboto', fontSize: '2em', fontWeight: '300' }}>
           Roboto-300 font family: příliš žluťoučký kůň úpěl dábelské kódy
         </div>
-        <div style={{ fontWeight: '300' }}>
+        <div style={{ fontWeight: '100', fontSize: '1em' }}>
+          Material Icons font:
           <MyIcon color="red">delete</MyIcon>
-          <MyIcon color="green" style={{ fontSize: '36px' }}>motorcycle</MyIcon>
+          <MyIcon color="green" style={{ fontSize: '4em' }}>motorcycle</MyIcon>
+        </div>
+        <div>
+          Font Awesome: <i className="fa fa-home text-danger" style={{ fontSize: '3em' }}></i>
+        </div>
+        <div>
+          Font Awesome in FontIcon: <FontIcon className="fa fa-bicycle" style={{ fontSize: '3em' }} />
         </div>
       </div>
     )
