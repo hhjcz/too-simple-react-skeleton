dedaf2bfc5ef36349b2a8dcce84cd69611bd670f
Jan Haering <hhj@centrum.cz>
2016-03-22 08:12:13 +0100

    fix: don't display all nepiOpy

--- src/shared/umistovani/NepiOpy.jsx
+++ src/shared/umistovani/NepiOpy.jsx
@@ -17,19 +17,20 @@ export default class NepiOpy extends React.Component {
     let { nepiOpy } = this.props
     nepiOpy = nepiOpy.toArray ? nepiOpy.toArray() : nepiOpy
     const uniqNepiOpy = uniqBy(nepiOpy, nepiOp => nepiOp.ixop)
+    const slicedNepiOpy = uniqNepiOpy.slice(0, 10)
 
-    const listItems = uniqNepiOpy.map(nepiOp =>
+    const listItems = slicedNepiOpy.map(nepiOp =>
       <div style={{ paddingLeft: '1em' }} key={nepiOp.ixop}>
         {nepiOp.ixop} {nepiOp.akronym} "{nepiOp.nazevFirmy}"
       </div>
     )
 
-    const divHeight = Math.max(Math.ceil(uniqNepiOpy.length / 2 * 1.6), 2)
+    const divHeight = Math.max(Math.ceil(slicedNepiOpy.length / 2 * 1.6), 2)
 
     return listItems.length > 0 ?
       <div style={{ display: 'flex', alignItems: 'center', paddingLeft: '2em' }}>
         <Avatar size={20} backgroundColor={muiColors.green300} color={muiColors.grey50}>
-          { listItems.length }
+          { uniqNepiOpy.length }
         </Avatar>
         <div style={{ display: 'flex', flexFlow: 'column wrap', justifyContent: 'center', height: `${divHeight}em` }}>
           { listItems }
