28c3bbe3a0d2b7a14bc2fe57e8021b2809ef3efe
Jan Haering <hhj@centrum.cz>
2016-03-29 10:51:38 +0200

    feat: style of nav bar

--- src/shared/app/Navigation.jsx
+++ src/shared/app/Navigation.jsx
@@ -2,20 +2,21 @@
 import React from 'react'
 import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, NavItem, NavbarBrand } from 'react-bootstrap'
+import MyIcon from '../lib/MyIcon'
 
 export default class Navigation extends React.Component {
   static propTypes = {};
 
   render() {
     return (
-      <Navbar inverse>
+      <Navbar>
         <Navbar.Collapse>
           <Nav bsStyle="pills">
             <NavbarBrand>Dohlestr ({process.env.NODE_ENV})</NavbarBrand>
-            <IndexLinkContainer to="/"><NavItem>Home</NavItem></IndexLinkContainer>
-            <LinkContainer to="/zarizeni"><NavItem>List</NavItem></LinkContainer>
-            <LinkContainer to="/umistovani"><NavItem>Umistovani</NavItem></LinkContainer>
-            <LinkContainer to="/hriste"><NavItem>Hriste</NavItem></LinkContainer>
+            <IndexLinkContainer to="/"><NavItem><MyIcon color="grey">home</MyIcon></NavItem></IndexLinkContainer>
+            <LinkContainer to="/zarizeni"><NavItem>Seznam</NavItem></LinkContainer>
+            <LinkContainer to="/umistovani"><NavItem>Umísťovaní</NavItem></LinkContainer>
+            <LinkContainer to="/hriste"><NavItem>Hřiště</NavItem></LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
