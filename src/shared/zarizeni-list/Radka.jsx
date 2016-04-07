56800ab8fe35209c2136a4e982026ae41b2a9602
Jan Haering <hhj@centrum.cz>
2016-04-07 16:15:26 +0200

    feat: indicated deleted zarizeni in list

--- src/shared/zarizeni-list/Radka.jsx
+++ src/shared/zarizeni-list/Radka.jsx
@@ -14,8 +14,9 @@ export default class Radka extends React.Component {
 
   render() {
     const { zarizeni, columns, pozice, highlighted } = this.props
+    const shadowed = zarizeni.deletedAt !== null
     return (
-      <div className={`myTableRow ${highlighted ? 'highlighted' : ''}`}>
+      <div className={`myTableRow ${highlighted ? 'highlighted' : ''} ${shadowed ? 'shadowed' : ''}`}>
         {
           columns.map(col => {    // eslint-disable-line arrow-body-style
             return col.visible ?
