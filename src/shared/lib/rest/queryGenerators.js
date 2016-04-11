4991f39fb78b7e6388c5536d80555e84bf60dc57
Jan Haering <hhj@centrum.cz>
2016-04-11 15:54:52 +0200

    feat: unify rest parameters casing (harder than it seemed to be)

--- src/shared/lib/rest/queryGenerators.js
+++ src/shared/lib/rest/queryGenerators.js
@@ -1,5 +1,4 @@
 /** Created by hhj on 1/28/16. */
-import snakeCase from 'lodash/snakeCase'
 
 function parseFilters(filters) {
 
@@ -9,23 +8,23 @@ function parseFilters(filters) {
     const comparator = filter.comparator || 'contains'
 
     if (comparator === 'contains' && filter.value !== null) {
-      suffix = '-lk'
+      suffix = 'Like'
       value = `%${filter.value}%`
     } else if (comparator === 'begins' && filter.value !== null) {
-      suffix = '-lk'
+      suffix = 'Like'
       value = `${filter.value}%`;
     } else if (comparator === 'empty' && filter.value === false) {
-      suffix = '-not'
+      suffix = 'NotEmpty'
       value = ''
     } else if (comparator === 'empty' && filter.value === true) {
-      suffix = '-null'
+      suffix = 'Empty'
       value = null
     } else if (filter.value !== null) {
       suffix = ''
       value = filter.value
     } else return null
 
-    result[snakeCase(filterName) + suffix] = value
+    result[filterName + suffix] = value
 
     return result
   }, {})
@@ -39,8 +38,8 @@ function collection(state, extraParams = {}) {
   let queryParams = {}
 
   if (page > 0) queryParams.page = page
-  if (perPage > 0) queryParams.per_page = perPage
-  if (sort && sort.by) queryParams.sort = `${sort.dir ? '-' : ''}${snakeCase(sort.by)}`
+  if (perPage > 0) queryParams.perPage = perPage
+  if (sort && sort.by) queryParams.sort = `${sort.dir ? '-' : ''}${sort.by}`
   if (filters && filters.forEach) queryParams = { ...queryParams, ...parseFilters(filters) }
   if (generalParams) queryParams = { ... queryParams, ...generalParams.toObject() }
 
@@ -50,7 +49,7 @@ function collection(state, extraParams = {}) {
 function collectionByIds(state, extraParams = {}) {
   const { ids, pagination, sort } = state
   const queryParams = { page: 1, per_page: 10000000 }
-  if (sort && sort.by) queryParams.sort = `${sort.dir ? '-' : ''}${snakeCase(sort.by)}`
+  if (sort && sort.by) queryParams.sort = `${sort.dir ? '-' : ''}${sort.by}`
 
   const pageStart = (pagination.page - 1) * pagination.perPage
   const pageEnd = pageStart + pagination.perPage
