405bd1ad04cd7e4d0d3cb534acfa9890bbfc408f
Jan Haering <hhj@centrum.cz>
2016-04-22 13:57:53 +0200

    test: remove "only"

--- src/shared/umistovani/__tests__/markUtils.spec.js
+++ src/shared/umistovani/__tests__/markUtils.spec.js
@@ -3,7 +3,7 @@
 import { expect } from 'chai'
 import * as markUtils from '../markUtils'
 
-describe.only('umistovani markUtils', () => {
+describe('umistovani markUtils', () => {
 
   it('should mark nepi op', () => {
     markUtils.markPotencialniNepiop('neco 123456 neco2 98765neco3')
