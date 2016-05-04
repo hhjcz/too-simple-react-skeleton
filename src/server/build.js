da82177ce9f4d69b95bf85d8a5c8da5606dd1848
Jan Haering <hhj@centrum.cz>
2016-05-04 09:27:09 +0200

    chore: improve webpack build error handling

--- src/server/build.js
+++ src/server/build.js
@@ -9,8 +9,24 @@ const compiler = webpack(config)
 
 console.info('Building webpack bundle...')
 
+function handleError(error) {
+  console.error('Webpack build error: ', error)
+  return error
+}
+
+function handleWarning(warning) {
+  console.warn('Webpack build warning: ', warning)
+  return warning
+}
+
 compiler.run((err, stats) => {
+  if (err) return handleError(err)
+  const jsonStats = stats.toJson()
+  if (jsonStats.errors.length > 0) return handleError(jsonStats.errors)
+  if (jsonStats.warnings.length > 0) handleWarning(jsonStats.warnings)
+
   if (!stats.hasErrors()) {
     console.log(stats.toString({ colors: true, chunks: false, modules: false, children: false }))
   }
+  return null
 })
