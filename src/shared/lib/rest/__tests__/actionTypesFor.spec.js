7df5b949f46ef5b66a31a0e622de1501309943e0
Jan Haering <hhj@centrum.cz>
2016-05-10 11:46:37 +0200

    refactor: move pagination, filter, sort actions to rest library

--- src/shared/lib/rest/__tests__/actionTypesFor.spec.js
+++ src/shared/lib/rest/__tests__/actionTypesFor.spec.js
@@ -4,7 +4,7 @@ import { actionTypesFor } from '../actionTypesFor'
 
 describe('actionTypesFor', () => {
 
-  it('should generate action types', () => {
+  it('should generate async action types', () => {
     const actionTypes = actionTypesFor('someResource')
     expect(typeof actionTypes.fetchIdsRequested).to.equal('string')
     expect(typeof actionTypes.fetchCollectionRequested).to.equal('string')
@@ -13,6 +13,14 @@ describe('actionTypesFor', () => {
     expect(typeof actionTypes.createSuccess).to.equal('string')
     expect(typeof actionTypes.updateError).to.equal('string')
     expect(typeof actionTypes.destroySuccess).to.equal('string')
+    expect(actionTypes.destroySuccess).to.equal('@my-rest/SOME_RESOURCE_DESTROY_SUCCESS')
+  })
+
+  it('should generate sync action types', () => {
+    const actionTypes = actionTypesFor('someResource')
+    expect(typeof actionTypes.gotoPage).to.equal('string')
+    expect(actionTypes.gotoPage).to.equal('@my-rest/SOME_RESOURCE_GOTO_PAGE')
+    expect(actionTypes.clearEntities).to.equal('@my-rest/SOME_RESOURCE_CLEAR_ENTITIES')
   })
 
 })
