8fbf7ace246b5c4ed05efa25ec3698ac511c1b03
Jan Haering <hhj@centrum.cz>
2016-05-31 15:39:36 +0200

    chore: clean up unused files

--- src/shared/zarizeni-list/core.js
+++
@@ -1,8 +0,0 @@
-/** Created by hhj on 12/18/15. */
-import { List } from 'immutable'
-
-import { ZarizeniFactory } from '../app/models/Zarizeni'
-
-export function setList(state, seznamZarizeni) {
-  return state.set('seznamZarizeni', List(seznamZarizeni).map(z => ZarizeniFactory(z)))
-}
