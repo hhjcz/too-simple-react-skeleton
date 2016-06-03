98cf1adf471840afcca59f7c08697c830e5b0a9a
Jan Haering <hhj@centrum.cz>
2016-06-03 12:34:33 +0200

    feat: navigation in zarizeni detail view

--- src/shared/zarizeni/Navigation.jsx
+++ src/shared/zarizeni/Navigation.jsx
@@ -2,9 +2,11 @@
 /* eslint-disable react/no-multi-comp */
 import React, { PropTypes } from 'react'
 import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import { Navbar, Nav, NavItem, Pagination } from 'react-bootstrap'
+import IconButton from 'material-ui/IconButton'
 import MyIcon from '../lib/MyIcon'
 import '../lib/icons.css'
+import colors from '../app/colors'
 
 class ColoredIcon extends React.Component {
   static propTypes = {
@@ -19,10 +21,14 @@ class ColoredIcon extends React.Component {
 export default class Navigation extends React.Component {
   static propTypes = {
     zarizeniId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    cursorAt: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    onCursorChange: PropTypes.func.isRequired,
+    reload: PropTypes.func.isRequired,
   };
 
   render() {
-    const { zarizeniId } = this.props
+    const { zarizeniId, cursorAt, total, onCursorChange, reload } = this.props
 
     return (
       <Navbar>
@@ -43,6 +49,32 @@ export default class Navigation extends React.Component {
                 <ColoredIcon>usb</ColoredIcon>
               </NavItem>
             </LinkContainer>
+            <IconButton
+              tooltip="previous"
+              onTouchTap={function() {
+                onCursorChange(cursorAt > 1 ? cursorAt - 1 : 1)
+              }}
+            >
+              <MyIcon color={colors.blueGrey600}>arrow_back</MyIcon>
+            </IconButton>
+
+            <IconButton tooltip="reload" onTouchTap={function() { reload() }}>
+              <MyIcon color={colors.blueGrey800}>autorenew</MyIcon>
+            </IconButton>
+
+            <IconButton
+              tooltip="next"
+              onTouchTap={function() {
+                onCursorChange(cursorAt < total ? cursorAt + 1 : cursorAt)
+              }}
+            >
+              <MyIcon color={colors.blueGrey800}>arrow_forward</MyIcon>
+            </IconButton>
+            <Pagination
+              items={total} activePage={cursorAt}
+              prev next first last ellipsis bsSize="small" maxButtons={9}
+              onSelect={function(eventKey) { onCursorChange(eventKey) }}
+            />
           </Nav>
         </Navbar.Collapse>
       </Navbar>
