169ef3dbc9f5a2036d2c7bdd370af599da9c243e
hhj-akutak <hhj@centrum.cz>
2016-08-12 19:08:46 +0200

    fix: footer tests

--- src/shared/lib/tabulka/__tests__/Footer.spec.js
+++ src/shared/lib/tabulka/__tests__/Footer.spec.js
@@ -5,16 +5,13 @@ import React from 'react'
 import sd from 'skin-deep'
 import Footer from '../Footer'
 
-describe.only('tabulka Footer component', () => {
+describe('tabulka Footer component', () => {
   let tree
   let vdom
-  let spyChange
   // let instance
 
   const shallowRender = (props) => {
-    spyChange = chai.spy()
     tree = sd.shallowRender(React.createElement(Footer, props))
-
     // instance = tree.getMountedInstance()
     vdom = tree.getRenderOutput()
     // console.log(vdom)
@@ -22,27 +19,29 @@ describe.only('tabulka Footer component', () => {
 
   it('should render with default props', () => {
     shallowRender()
-    expect(vdom.type).to.equal('div')
     // expect(vdom.props.children.type).to.equal('');
   })
 
-  it('should handle change of perPage input', () => {
-    shallowRender({ onRowCountChange: spyChange })
+  it('should handle change of rowCount input', () => {
+    const spyChange = chai.spy()
+    shallowRender({ onRowCountChange: spyChange, debounce: 1 })
     const perPageInput = tree.subTree('#rowCountInput').getRenderOutput()
-    perPageInput.props.onChange({ target: { value: '66' } })
-    console.log('Called')
-    expect(spyChange).to.be.called.once.with(66)
+    return perPageInput.props.onChange({ target: { value: '66' } }).then(value => {
+      expect(spyChange).to.be.called.once.with(66)
+    })
   })
 
-  // it('should call perPage change handler', () => {
-  //   tree.fillField('#perPageInput', '77')
-  //   expect(spyChange).to.be.called.once.with(77)
-  // })
+  it('should call rowCount change handler', () => {
+    const spyChange = chai.spy()
+    shallowRender({ onRowCountChange: spyChange, debounce: 0 })
+    tree.fillField('#rowCountInput', '77')
+    expect(spyChange).to.be.called.once.with(77)
+  })
 
-  // it('should set input style to error', () => {
-  //   tree = sd.shallowRender(React.createElement(Paginator, { pagination: { perPage: 'abc' } }))
-  //   const perPageFormGroup = tree.getRenderOutput().props.children.props.children[2].props.children
-  //   expect(perPageFormGroup.props.validationState).to.equal('error')
-  // }
+  it('should set input style to error', () => {
+    shallowRender({ debounce: 0, rowCount: 'abc' })
+    const rowCountFormGroup = tree.subTree('#rowCountFG').getRenderOutput()
+    expect(rowCountFormGroup.props.validationState).to.equal('error')
+  })
 
 })
