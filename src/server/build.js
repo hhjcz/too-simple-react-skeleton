7f3c215e24900d2d4d93e90160938a26039682f0
Jan Haering <hhj@centrum.cz>
2016-09-14 15:31:19 +0200

    chore: revert build skip

--- src/server/build.js
+++ src/server/build.js
@@ -7,10 +7,11 @@ const config = require('./webpack.prod')
 
 const compiler = webpack(config)
 
-if (process.env.NODE_ENV === 'development') {
-  console.info(`Skipping webpack build in development mode... (NODE_ENV=${process.env.NODE_ENV})`)
-  return
-}
+// FIXME - not working with webpack-isomorphic tools, which is waiting forever for first build to finish
+// if (process.env.NODE_ENV === 'development') {
+//   console.info(`Skipping webpack build in development mode... (NODE_ENV=${process.env.NODE_ENV})`)
+//   return
+// }
 
 console.info(`Building webpack bundle... (NODE_ENV=${process.env.NODE_ENV})`)
 
