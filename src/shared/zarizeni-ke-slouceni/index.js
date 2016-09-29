b78639a8f56f747898e60627a6bb819213c14557
Jan Haering <hhj@centrum.cz>
2016-09-29 08:56:30 +0200

    refactor: extracted grid container to react-lib package

--- src/shared/zarizeni-ke-slouceni/index.js
+++ src/shared/zarizeni-ke-slouceni/index.js
@@ -1,12 +1,13 @@
 /** Created by hhj on 8/31/16. */
 import { connect } from 'react-redux'
 import { reduxUtils } from '@hhjcz/js-lib'
+import { MyGridContainer as Container } from '@hhjcz/react-lib'
 import rest from '../app/rest'
-import Container from './Container'
 import * as actions from './actions'
 
 export default connect(
   (state) => ({
+    title: 'zarizeni-ke-slouceni',
     ...(rest.selectResource('zarizeniKeSlouceni')(state)),
     ...(state.zarizeniKeSlouceni.toObject())
   }),
