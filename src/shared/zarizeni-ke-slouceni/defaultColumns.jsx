50d2c9fc63d63023b9560a4cc3a837fc1d46d804
Jan Haering <hhj@centrum.cz>
2016-09-29 16:27:57 +0200

    style: display columns

--- src/shared/zarizeni-ke-slouceni/defaultColumns.jsx
+++ src/shared/zarizeni-ke-slouceni/defaultColumns.jsx
@@ -2,43 +2,60 @@
 /* eslint-disable no-nested-ternary */
 import { Map } from 'immutable'
 import React from 'react'
+import { Link } from 'react-router'
 import { Column, columntValueTypes } from '@hhjcz/react-lib/lib/tabulka'
 
 const defaultColumns = Map(
   {
     firstId: new Column({
       name: 'firstId',
-      caption: 'First ID',
+      caption: 'ID #1',
       valueType: columntValueTypes.number,
       position: 10,
       width: 1,
     }),
     secondId: new Column({
       name: 'secondId',
-      caption: 'Second ID',
+      caption: 'ID #2',
       valueType: columntValueTypes.number,
       position: 20,
       width: 1,
     }),
     firstIpAddress: new Column({
       name: 'firstIpAddress',
-      caption: 'IP address',
+      caption: 'IP adresa',
       valueType: columntValueTypes.ipAddress,
       position: 30,
+      width: 1,
     }),
     firstName: new Column({
       name: 'firstName',
-      caption: 'First name',
+      caption: 'Jmeno #1',
       valueType: columntValueTypes.string,
       position: 40,
       width: 4,
     }),
     secondName: new Column({
       name: 'secondName',
-      caption: 'Second name',
+      caption: 'Jmeno #2',
       valueType: columntValueTypes.string,
       position: 50,
       width: 4,
+    }),
+    slouceni: new Column({
+      name: 'slouceni',
+      caption: 'Lze sloucit',
+      valueType: columntValueTypes.string,
+      position: 60,
+      width: 1,
+      sortable: false,
+      render: zarizeni => (
+        <div>
+          <Link to={`/udrzba/zarizeni_ke_slouceni/${zarizeni.firstId}/${zarizeni.secondId}`}>
+            {zarizeni.firstName.toLowerCase() === zarizeni.secondName.toLowerCase() ? 'OK' : '?'}
+          </Link>
+        </div>
+      ),
     }),
   }
 )
